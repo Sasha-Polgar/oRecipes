44c0b6861907c003c4d0cedec0b220be8e57b8d1
Sasha-Polgar <sasha.polgar@hotmail.com>
2023-11-14 21:50:05 +0100

    feat: tentative d'appel API, retour dessus demain matin

--- src/routes/Recipe/index.tsx
+++ src/routes/Recipe/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable arrow-body-style */
 import { Navigate, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import Page from '../../components/Page';
 import AppHeader from '../../components/AppHeader';
@@ -8,16 +9,22 @@ import Header from './Header';
 import Ingredients from './Ingredients';
 import Instructions from './Instructions';
 
-import { useAppSelector } from '../../hooks/redux';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { findRecipe } from '../../store/selectors/recipes';
 
 import './styles.scss';
+import { fetchRecipes } from '../../store/reducers/recipes';
 
 function Recipe() {
   const { slug } = useParams();
   if (!slug) {
     throw new Error('Aucun slug fourni');
   }
+  const dispatch = useAppDispatch;
+
+  useEffect(() => {
+    dispatch(fetchRecipes());
+  }, [dispatch]);
   const recipe = useAppSelector((state) =>
     findRecipe(state.recipes.list, slug)
   );
--- src/store/reducers/recipes.ts
+++ src/store/reducers/recipes.ts
@@ -1,18 +1,47 @@
-import { createSlice } from '@reduxjs/toolkit';
-import data from '../../data';
+import axios from 'axios';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { Recipe } from '../../@types/recipe';
 
 interface RecipesState {
   list: Recipe[];
+  isLoading: boolean;
+  error: string | null;
 }
 export const initialState: RecipesState = {
-  list: data,
+  list: [],
+  isLoading: false,
+  error: null,
 };
+
+export const fetchRecipes = createAsyncThunk(
+  'recipes/fetchRecipes',
+  async () => {
+    const { data } = await axios.get(
+      'https://orecipes-api.onrender.com/api/recipes'
+    );
+    return data;
+  }
+);
 
 const recipesReducer = createSlice({
   name: 'recipes',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchRecipes.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchRecipes.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.list = action.payload;
+      })
+      .addCase(fetchRecipes.rejected, (state) => {
+        state.error = 'Une erreur est survenue';
+        state.isLoading = false;
+      });
+  },
 });
 
 export default recipesReducer.reducer;
