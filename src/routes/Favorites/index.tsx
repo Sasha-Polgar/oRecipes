e12cbc295c6fd864b76920ca76e531ed318543b1
Sasha-Polgar <sasha.polgar@hotmail.com>
2023-11-17 12:06:21 +0100

    feat: preparation de la requete pour recuperer les recettes favorites

--- src/routes/Favorites/index.tsx
+++ src/routes/Favorites/index.tsx
@@ -1,11 +1,20 @@
 import { Navigate } from 'react-router-dom';
-import { useAppSelector } from '../../hooks/redux';
+import { useEffect } from 'react';
+import { useAppSelector, useAppDispatch } from '../../hooks/redux';
 import Page from '../../components/Page';
 import AppHeader from '../../components/AppHeader';
 import Content from '../../components/Content';
+import { fetchFavoriteRecipes } from '../../store/reducers/recipes';
 
 function Favorites() {
+  const dispatch = useAppDispatch();
   const isLogged = useAppSelector((state) => state.user.logged);
+
+  // Lorsque j'arrive sur la page, j'emet l'intention de récupérer mes recettes favorites
+  useEffect(() => {
+    dispatch(fetchFavoriteRecipes());
+  }, [dispatch]);
+
   // Si je ne suis pas connecté, je redirige vers la page d'accueil
   if (!isLogged) {
     return <Navigate to="/" />;
